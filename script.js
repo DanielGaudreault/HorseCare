b7eb8128b441ed56e81947c801d1c49292064ec2
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2025-08-06 11:29:51 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        const sectionId = this.getAttribute('href');
+        document.querySelector(sectionId).scrollIntoView({ behavior: 'smooth' });
+    });
+});
